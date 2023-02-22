aa29f8c2871ea79dce37c74f12f4a34ea0c12780
ionathan sideras <119215357+ionathansideras@users.noreply.github.com>
2023-02-22 21:40:22 +0200

    Game

--- My-Projects/Rock-Paper-Cissors/program.js
+++ My-Projects/Rock-Paper-Cissors/program.js
@@ -1,97 +1,96 @@
-//it return a random value from the computer
-function getComputerChoice(){
-    let random = Math.floor(Math.random() * 3);
-    if (random == 0){
-        random = 'rock'
-    }
-    else if(random == 1){
-        random = 'paper'
-    }
-    else{
-        random = 'cissors'
-    }
-    return random;
-};
-
-//asks the player for a value
-function playersChoice(){
-    let playersinput = prompt("Rock Paper Cissors!");
-    return playersinput;
-};
-
-//it returns a winner of one round
-function playRound(computerSelection, playerSelection){
-    let playerinput = playerSelection.toLowerCase();
-    let computerValue = computerSelection;
-    //it returns who won this round
-    if(computerValue == 'paper' && playerinput == 'paper'){
-        return "this is a tie!";
-    }
-    else if(computerValue == 'rock' && playerinput == 'rock'){
-        return "this is a tie!";
-    }
-    else if(computerValue == 'cissors' && playerinput == 'cissors'){
-        return "this is a tie!";
-    }
-    else if(computerValue == 'cissors' && playerinput == 'rock'){
-        return "Computer +1";
-    }
-    else if(computerValue == 'rock' && playerinput == 'cissors'){
-        return "Player +1";
-    }
-    else if(computerValue == 'rock' && playerinput == 'paper'){
-        return "Player +1";
-    }
-    else if(computerValue == 'paper' && playerinput == 'rock'){
-        return "Computer +1";
-    }
-    else if(computerValue == 'paper' && playerinput == 'cissors'){
-        return "Player +1";
-    }
-    else if(computerValue == 'cissors' && playerinput == 'paper'){
-        return "Computer +1";
-    }
-};
-
-//this function creates a 5 round game and declares a winner!!!
-function game(){
-    //veriables to keep track of points
-    let computerCount = 0;
-    let playerCount = 0;
-
-    //it runs the playerRound function 5 times
-    for(let i = 1; i <= 5; i++){
-
-        // i call those functions inside the for loop paseed in to veriables becuse i need them to run 5 times 
-        let computerSelection = getComputerChoice();
-        let playerSelection = playersChoice();
-        let track = playRound(computerSelection, playerSelection);
-
-        if(track === "Computer +1"){
-            //add a point for the computer
-            computerCount++;
-        }
-        else if(track === "Player +1"){
-            //add a point for the player
-            playerCount++;
-        }
+document.addEventListener('DOMContentLoaded', function(){
+   
+    let buttons = document.querySelectorAll('button');
+    let playerScore = 0;
+    let computerScore = 0;
+    let tie = 0;
+    for(let i = 0; i < buttons.length; i++){
         
-        console.log(`Round ${i}:`,`Player ${playerCount} - ${computerCount} Computer`,` = ${track}`);
-    }
+        buttons[i].onclick = () => {
+            let player = buttons[i].innerHTML;
 
-    //checks who has more points and returns a winner 
-    if(computerCount > playerCount){
-        console.log('Computer Wins!');
-    }
-    else if(computerCount < playerCount){
-        console.log('You Win!');
-    }
-    else {
-        console.log('Its Tie!');
-    }
+            let random = Math.floor(Math.random() * 3);
+            if (random == 0){
+                random = 'Rock'
+            }
+            else if(random == 1){
+                random = 'Paper'
+            }
+            else{
+                random = 'Cissors'
+            }
 
-    console.log(`Final score: Player ${playerCount} - ${computerCount} Computer`);
-};
+            function roundWinner(playerinput,computerSelection){
+                let computerValue = computerSelection;
+                //it returns who won this round
+                if(computerValue == 'Paper' && playerinput == 'Paper'){
+                    return "Tie";
+                }
+                else if(computerValue == 'Rock' && playerinput == 'Rock'){
+                    return "Tie";
+                }
+                else if(computerValue == 'Cissors' && playerinput == 'Cissors'){
+                    return "Tie";
+                }
+                else if(computerValue == 'Cissors' && playerinput == 'Rock'){
+                    return "Computer +1";
+                }
+                else if(computerValue == 'Rock' && playerinput == 'Cissors'){
+                    return "Player +1";
+                }
+                else if(computerValue == 'Rock' && playerinput == 'Paper'){
+                    return "Player +1";
+                }
+                else if(computerValue == 'Paper' && playerinput == 'Rock'){
+                    return "Computer +1";
+                }
+                else if(computerValue == 'Paper' && playerinput == 'Cissors'){
+                    return "Player +1";
+                }
+                else if(computerValue == 'Cissors' && playerinput == 'Paper'){
+                    return "Computer +1";
+                }
+            };
 
-//call the game function
-game();
+            let round = roundWinner(random,player);
+
+            function game(){
+                if(round == "Player +1"){
+                    playerScore++;
+                }
+                else if(round == "Computer +1"){
+                    computerScore++;
+                }
+                else{
+                    tie++;
+                }
+            };
+            game();
+
+            document.querySelector('#playerScore').innerHTML = `Your Score: ${playerScore}`;
+            document.querySelector('#computersScore').innerHTML = `Computer Score: ${computerScore}`;
+            document.querySelector('#round').innerHTML = round;
+            
+            if(playerScore >= 5 || computerScore >= 5){
+                if(playerScore > computerScore){
+                    document.querySelector('h3').innerHTML = 'You Win!';
+                }
+                else if(playerScore < computerScore){
+                    document.querySelector('h3').innerHTML = 'You Lost!';
+                }
+                else {
+                    document.querySelector('h3').innerHTML = 'Its Tie!';
+                }
+            }
+            
+            if(playerScore >= 6 || computerScore >= 6){
+                playerScore = 0;
+                computerScore = 0;
+                document.querySelector('#playerScore').innerHTML = `Your Score: ${playerScore}`;
+                document.querySelector('#computersScore').innerHTML = `Computer Score: ${computerScore}`;
+                document.querySelector('#round').innerHTML = 'Points';
+                document.querySelector('h3').innerHTML = '';
+            }
+        };
+    };
+});
