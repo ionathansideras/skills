f5b811a8515664af388e9722150bbc8c4d50b938
ionathan sideras <119215357+ionathansideras@users.noreply.github.com>
2023-03-08 21:46:26 +0200

    books

--- My-Projects/Books/program.js
+++ My-Projects/Books/program.js
@@ -9,22 +9,46 @@ function addBookToLibrary(title, author, pages, read) {
     // do stuff here
     this.title = title;
     this.author = author;
-    this.pager = pages;
+    this.pages = pages;
     this.read = read;
 }
 
 function loop(array){
     for(let i = 0; i < array.length; i++){
-        console.log(array[i]);
+        const obj = array[i];
+
+        let main = document.querySelector('main');
+        let artical = document.createElement('article');
+
+        for(let j in obj){
+            let addIt = document.createElement('div');
+            addIt.innerHTML = obj[j];
+            artical.appendChild(addIt);
+            main.appendChild(artical);
+        }
+
+        let button = document.createElement('button');
+        button.innerHTML = 'Remove';
+        button.setAttribute('value',i);
+        button.setAttribute('class', 'but');
+        artical.appendChild(button);
     }
 };
+
+function clear(){
+    const articles = document.querySelectorAll('article');
+    for (let i = 0; i < articles.length; i++) {
+        articles[i].remove();
+    }
+};
+
 
 // elenxoume to form otan kanei submit
 document.querySelector('form').onsubmit = () => {
     // pernoume ta inputs values 
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
-    const pages = document.querySelector('#pages').value;
+    const pages = document.querySelector('#pages').value+' Pages';
     const read = document.querySelector('#read');
     let check = '';
     if (read.checked) {
@@ -40,32 +64,27 @@ document.querySelector('form').onsubmit = () => {
     document.querySelector('#pages').value = '';
     document.querySelector('#read').checked = false;
 
-    let main = document.querySelector('main');
-    let article = document.createElement('article');
-    
+    clear();
+    loop(myLibrary);
 
-    const divTitle = document.createElement('div');
-    divTitle.innerHTML = title;
-    article.appendChild(divTitle);
-
-    const divAuthor = document.createElement('div');
-    divAuthor.innerHTML = author;
-    article.appendChild(divAuthor);
-
-    const divPages = document.createElement('div');
-    divPages.innerHTML = pages;
-    article.appendChild(divPages);
-
-    const divCheck = document.createElement('div');
-    divCheck.innerHTML = check;
-    article.appendChild(divCheck);
-
-    const deleteBut = document.createElement('button');
-    deleteBut.innerHTML = 'Remove';
-    article.appendChild(deleteBut);
-
-    main.appendChild(article);
+    remove();
 
     //stop submiting
     return false;
 }
+//kanei remove to object pou theloume
+function remove(){
+    let remove = document.querySelectorAll('.but');
+    remove.forEach(function(button){
+        button.addEventListener('click', function(){
+            myLibrary.splice(button.value, 1);
+            clear();
+            loop(myLibrary);
+        })
+    })
+}
+
+
+
+
+
