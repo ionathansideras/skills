ae2d08e2c0a132595b3ede315af05c56c14837c1
ionathan sideras <119215357+ionathansideras@users.noreply.github.com>
2023-03-13 15:02:20 +0200

    practise

--- My-Projects/Tic-tac-toe/program.js
+++ My-Projects/Tic-tac-toe/program.js
@@ -1,14 +1,53 @@
-const game = function(){
+// creates the gameboard
+const board = (function(){
+    const gameboard = ['','','','','','','','',''];
+    return {gameboard};
+}());
 
-    (function(){
-        const main = document.createElement('main');
-        for (let i = 0; i < 3*3; i++) {
-            const div = document.createElement('div');
-            main.appendChild(div);
-        }
-        const body = document.querySelector('body');
-        body.appendChild(main);
-    } ());
-
+// creates a player
+const player = function(name, letter){
+    return {name, letter};
 };
-game();
+
+const player1 = player('bob', 'x');
+const player2 = player('carl', 'o');
+
+// controls the flow of the game 
+const flow = (function(player1, player2){
+    let score = 0;
+
+        
+     
+})();
+
+// display the board in the screen
+function display(e){
+    for(let i = 0; i < e.gameboard.length; i++){
+        const main = document.querySelector('main');
+        const div = document.createElement('div');
+        div.innerHTML = e.gameboard[i];
+        div.setAttribute('value', i);
+        main.appendChild(div); 
+    }
+    
+    const check = document.querySelectorAll('div');
+    check.forEach(function(button){
+        button.addEventListener('click', function(){
+            let butVal = button.getAttribute('value');
+            console.log(butVal);
+            board.gameboard[butVal] = butVal;
+            clear();
+            display(board);
+        });
+    });
+};
+display(board);
+
+// it clears the screen
+function clear(){
+    const div = document.querySelectorAll("div");
+    for (let i = 0; i < div.length; i++) {
+    div[i].remove();
+  }
+};
+
