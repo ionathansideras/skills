ecaf4f179a78eb8ad50766cdbbc2576ee1b0662a
ionathan sideras <119215357+ionathansideras@users.noreply.github.com>
2023-03-13 16:16:24 +0200

    ugly code

--- My-Projects/Tic-tac-toe/program.js
+++ My-Projects/Tic-tac-toe/program.js
@@ -1,47 +1,106 @@
-// creates the gameboard
-const board = (function(){
-    const gameboard = ['','','','','','','','',''];
-    return {gameboard};
+// creates the board
+const gameBoard = (function(){
+    let Gameboard = ['','','','','','','','',''];
+    return {Gameboard};
 }());
 
-// creates a player
-const player = function(name, letter){
-    return {name, letter};
-};
+// creates the players
+const players = function(name, e){
+    // methods to get name and X or O
+    const getName = () => name;
+    const getE = () => e;
 
-const player1 = player('bob', 'x');
-const player2 = player('carl', 'o');
+    return {getE, getName};
+}
 
-// controls the flow of the game 
-const flow = (function(player1, player2){
-    let score = 0;
-
-        
-     
-})();
-
-// display the board in the screen
+//dysplayes everything in the screen
 function display(e){
-    for(let i = 0; i < e.gameboard.length; i++){
+    for(let i = 0; i < e.Gameboard.length; i++){
         const main = document.querySelector('main');
         const div = document.createElement('div');
-        div.innerHTML = e.gameboard[i];
+        div.innerHTML = e.Gameboard[i];
         div.setAttribute('value', i);
         main.appendChild(div); 
     }
-    
+    check();
+}
+display(gameBoard);
+
+const player1 = players('bob', 'x');
+const player2 = players('carl', 'o');
+let flag = true;
+
+// when on click something happends
+function check(){
     const check = document.querySelectorAll('div');
     check.forEach(function(button){
+       
         button.addEventListener('click', function(){
             let butVal = button.getAttribute('value');
-            console.log(butVal);
-            board.gameboard[butVal] = butVal;
+            //puts the players marker in the gameboard
+            if ((gameBoard.Gameboard[butVal] == '') && flag == true){
+                gameBoard.Gameboard[butVal] = player1.getE();
+                console.log(gameBoard.Gameboard[butVal]);
+                flag = false;
+            }
+            if ((gameBoard.Gameboard[butVal] == '') && flag == false){
+                gameBoard.Gameboard[butVal] = player2.getE();
+                console.log(gameBoard.Gameboard[butVal]);
+                flag = true;
+            }
+            getWinner();
+            //clears everything
             clear();
-            display(board);
+            //display the new updated board
+            display(gameBoard);
         });
     });
+}
+
+function getWinner(){
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[3] && gameBoard.Gameboard[6])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[3] && gameBoard.Gameboard[6])  == 'o'){
+        console.log('ok');
+    }
+    //
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[1] && gameBoard.Gameboard[2])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[1] && gameBoard.Gameboard[2])  == 'o'){
+        console.log('ok');
+    }
+    //
+    if ((gameBoard.Gameboard[2] && gameBoard.Gameboard[5] && gameBoard.Gameboard[8])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[2] && gameBoard.Gameboard[5] && gameBoard.Gameboard[8])  == 'o'){
+        console.log('ok');
+    }
+    //
+    if ((gameBoard.Gameboard[6] && gameBoard.Gameboard[7] && gameBoard.Gameboard[8])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[6] && gameBoard.Gameboard[7] && gameBoard.Gameboard[8])  == 'o'){
+        console.log('ok');
+    }
+    //
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[4] && gameBoard.Gameboard[8])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[0] && gameBoard.Gameboard[4] && gameBoard.Gameboard[8])  == 'o'){
+        console.log('ok');
+    }
+    //
+    if ((gameBoard.Gameboard[2] && gameBoard.Gameboard[4] && gameBoard.Gameboard[6])  == 'x'){
+        console.log('ok');
+    }
+    if ((gameBoard.Gameboard[2] && gameBoard.Gameboard[4] && gameBoard.Gameboard[6])  == 'o'){
+        console.log('ok');
+    }
+
 };
-display(board);
 
 // it clears the screen
 function clear(){
